7f613226f326cf3183155d27f7b949e93b244793
evandro.santana <evandro.santana@sead.pi.gov.br>
2025-05-29 11:18:39 -0300

    add filter OR

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createUser } from '../services/user';
+import { createUser, createUsers, getAllUsers, getUserByEmail } from '../services/user';
 
 export const mainRouter = Router();
 
@@ -9,12 +9,40 @@ mainRouter.get('/ping', (req, res) => {
 
 mainRouter.post('/user', async (req, res) => {
     const user = await createUser({
-        name: 'Testador 1', 
-        email: 'teste1@hotmail.com'
+        name: 'Testador 2', 
+        email: 'teste2@hotmail.com',
+        Posts: {
+            create: {
+                title: 'Titulo de teste do testador 2',
+                body: 'Corpo de teste do testador 2',
+
+            }
+        }
     });
     if(user ) {
     res.status(201).json({ user });
     } else {
         res.status(500).json({ error: "Ocorreu um erro ao criar o usuário"});
     }
+})
+
+mainRouter.post('/users', async (req, res ) => {
+    const result = await createUsers([        
+      { name: 'João', email: 'joao@hotmail.com'},
+      { name: 'Maria', email: 'maria@hotmail.com'},
+      { name: 'José', email: 'jose@gmail.com'},
+      { name: 'Ana', email: 'ana@gmail.com'},
+      { name: 'Pedro', email: 'pedro@gmail.com'},
+    ]);
+    res.json({ result });
+})
+
+mainRouter.get('/users', async (req, res) => {
+    const result = await getAllUsers();
+    res.json({ result});
+})
+
+mainRouter.get('/user', async (req, res) => {
+    const result = await getUserByEmail('joao@hotmail.com');
+    res.json({ result});
 })
--- src/services/user.ts
+++ src/services/user.ts
@@ -3,8 +3,56 @@ import { prisma } from "../libs/prisma"
 
 export const createUser = async (data: Prisma.UserCreateInput) => { 
   try {
-    return await prisma.user.create({data});
+    return await prisma.user.create({ data });
   } catch (error) {
     return false;
   }
+}
+
+export const createUsers = async (users: Prisma.UserCreateInput[]) => {
+  try { 
+    return await prisma.user.createMany({
+      data: users,
+      skipDuplicates: true
+    });
+  } catch(error) {
+    return false;
+  }
+}
+
+export const getAllUsers = async () => {
+  const users = await prisma.user.findMany({ 
+    where: {
+     OR: [
+      {
+        email: {
+          endsWith: 'teste1@hotmail.com'
+        }        
+      },
+      {
+        email: {
+          endsWith: 'teste2@hotmail.com'
+        }
+      }
+     ]
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      status: true,      
+    }
+  });
+  return users;
+}
+
+export const getUserByEmail = async (email: string) => {
+  const user = await prisma.user.findFirst({
+    where: { email },
+    select: {
+      id: true,
+      name: true
+    }
+  });
+  return user;
 }
